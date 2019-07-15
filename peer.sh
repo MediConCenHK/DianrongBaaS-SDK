610db4dbe3d46bcb7319f6ad37e7f5b03ac41345
davidkhala <david-khala@hotmail.com>
2019-07-15 15:50:08 +0800

    reference peer.sh

--- peer.sh
+++ peer.sh
@@ -7,4 +7,12 @@ export CORE_PEER_TLS_CERT_FILE=/etc/hyperledger/fabric/tls/server.crt
 export CORE_PEER_TLS_KEY_FILE=/etc/hyperledger/fabric/tls/server.key
 export CORE_PEER_TLS_ROOTCERT_FILE=/etc/hyperledger/fabric/tls/ca.crt
 
-$1
+remain_params=""
+for ((i = 2; i <= ${#}; i++)); do
+	j=${!i}
+	remain_params="$remain_params $j"
+done
+
+peerSDK="curl --silent --show-error https://raw.githubusercontent.com/davidkhala/fabric-common/release-1.4/bash/peer.sh"
+$peerSDK | bash -s $1 $remain_params
+
