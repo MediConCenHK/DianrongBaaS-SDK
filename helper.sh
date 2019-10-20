96122a05df256d0010bd3fe4e0d5cfb09cb757a6
david <david-khala@hotmail.com>
2019-10-20 15:11:14 +0800

    add linkOrderer

--- helper.sh
+++ helper.sh
@@ -1,36 +1,39 @@
 #!/usr/bin/env bash
 set -e
 linkPeer() {
-    sudo ln -s /home/setup/config/peer/peer /usr/bin/peer
+	sudo ln -s /home/setup/config/peer/peer /usr/bin/peer
+}
+linkOrderer() {
+	sudo ln -s /home/setup/config/orderer/orderer /usr/bin/orderer
 }
 peerLog() {
-    tail -f /data/hyperledger/log/peer.log
+	tail -f /data/hyperledger/log/peer.log
 }
-ordererLog(){
-    tail -f /data/hyperledger/log/orderer.log 
+ordererLog() {
+	tail -f /data/hyperledger/log/orderer.log
 }
 
 disableDockerAutoUpgrade() {
-    local NotToUpgradeList=/etc/apt/apt.conf.d/50unattended-upgrades
-    echo "To disable docker auto upgrade"
+	local NotToUpgradeList=/etc/apt/apt.conf.d/50unattended-upgrades
+	echo "To disable docker auto upgrade"
 
-    if ! docker --version ; then
-        echo "Skipped, docker is not installed"
-        return
-    fi
+	if ! docker --version; then
+		echo "Skipped, docker is not installed"
+		return
+	fi
 
-    if [[ ! -f "$NotToUpgradeList" ]]; then
-        echo "Skipped, because /etc/apt/apt.conf.d/50unattended-upgrades not exist"
-        return
-    fi
+	if [[ ! -f "$NotToUpgradeList" ]]; then
+		echo "Skipped, because /etc/apt/apt.conf.d/50unattended-upgrades not exist"
+		return
+	fi
 
-    if sudo grep "docker" ${NotToUpgradeList}; then
-        echo "Skipped, because already added docker into upgrade package-blacklist in ${NotToUpgradeList}"
-        return
-    fi
+	if sudo grep "docker" ${NotToUpgradeList}; then
+		echo "Skipped, because already added docker into upgrade package-blacklist in ${NotToUpgradeList}"
+		return
+	fi
 
-    sudo sed -i '/Unattended-Upgrade::Package-Blacklist/a\    "docker";' /etc/apt/apt.conf.d/50unattended-upgrades
-    echo "Added docker into upgrade package-blacklist in ${NotToUpgradeList}"
+	sudo sed -i '/Unattended-Upgrade::Package-Blacklist/a\    "docker";' /etc/apt/apt.conf.d/50unattended-upgrades
+	echo "Added docker into upgrade package-blacklist in ${NotToUpgradeList}"
 }
 
 $1
