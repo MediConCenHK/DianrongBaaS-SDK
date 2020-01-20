df7ace29c8b15043bf6b3712d49101203fa4d3d7
david <david-khala@hotmail.com>
2020-01-20 16:28:32 +0800

    0.4.0: reorder param list add transactionOnPeers

--- nodejs/transaction.js
+++ nodejs/transaction.js
@@ -5,32 +5,32 @@ const EventHub = require('khala-fabric-sdk-node/eventHub');
 const Config = require('./configHelper');
 exports.queryOnPeers = async (channelName, chaincodeId, fcn, args = [], transientMap, userID, targetPeers) => {
 	const client = await Config.getClientOfUser(userID);
-	const resp = await transactionProposal(client, targetPeers, channelName, {
+	return await transactionProposal(client, targetPeers, channelName, {
 		chaincodeId, fcn, args, transientMap
 	});
-	return resp;
 };
 
-exports.query = async (channelName, chaincodeId, fcn, args, transientMap, endorserFilter, userID) => {
+exports.query = async (channelName, chaincodeId, fcn, args, transientMap, userID, endorserFilter) => {
 	const targetPeers = await Config.getActivePeers(endorserFilter);
-	const resp = await exports.queryOnPeers(channelName, chaincodeId, fcn, args, transientMap, userID, targetPeers);
-	return resp;
+	return await exports.queryOnPeers(channelName, chaincodeId, fcn, args, transientMap, userID, targetPeers);
 };
-exports.transaction = async (channelName, chaincodeId, fcn, args = [], transientMap, endorserFilter, userID) => {
-
-	const activePeers = await Config.getActivePeers(endorserFilter);
+exports.transactionOnPeers = async (channelName, chaincodeId, fcn, args = [], transientMap, userID, targetPeers) => {
 	const channel = await prepareChannel(channelName, userID, false);
 	const client = channel._clientContext;
 
-	const eventHubs = activePeers.map(peer => new EventHub(channel, peer));
+	const eventHubs = targetPeers.map(peer => new EventHub(channel, peer));
 
 	for (const eventHub of eventHubs) {
 		await eventHub.connect();
 	}
 	const orderers = await Config.getActiveOrderers();
 	const orderer = orderers[0];
-	const resp = await invoke(client, channelName, activePeers, eventHubs,
+	return await invoke(client, channelName, targetPeers, eventHubs,
 		{chaincodeId, fcn, args, transientMap},
 		orderer);
-	return resp;
+};
+exports.transaction = async (channelName, chaincodeId, fcn, args, transientMap, userID, endorserFilter) => {
+	const activePeers = await Config.getActivePeers(endorserFilter);
+	return await exports.transactionOnPeers(channelName, chaincodeId, fcn, args, transientMap, userID, activePeers);
+
 };
